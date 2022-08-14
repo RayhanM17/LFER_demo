f7fc4835884b78be6098c49b92fa5051020d3032
unknown <raysdigitalservice@gmail.com>
2022-08-14 19:19:13 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -6,10 +6,12 @@ menuBtn.addEventListener('click', (e) => {
   if(e.target.classList.contains('bi-list')) {
     e.target.classList.replace('bi-list', 'bi-x')
     list.classList.replace('top-[-400px]', 'top-[100px]')
+    list.classList.replace('z-[-1]', 'z-[1]')
     list.classList.replace('opacity-0', 'opacity-100')
   } else {
     e.target.classList.replace('bi-x', 'bi-list')
     list.classList.replace('top-[100px]', 'top-[-400px]')
+    list.classList.replace('z-[1]', 'z-[-1]')
     list.classList.replace('opacity-100', 'opacity-0')
   }
 })
